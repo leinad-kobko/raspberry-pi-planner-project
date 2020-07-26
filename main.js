41087572a7ad7fbceae7f6d4e6b14749e8b7413f
leinadkobko <danielkobko@gmail.com>
2020-07-26 17:59:45 -0400

    added old files

---
+++ main.js
@@ -0,0 +1,61 @@
+/********************************************************************************************************
+Author: Daniel Kobko
+Date of Start:
+Date of Creation: ---
+
+Purpose {
+A main program compile together the functionality and act as a hub for all tasks to be viewed together.
+
+It is a program that acts as an organizer for someone to manage their:
+    - Meal plan for the day
+    - Workout for the day
+    - Plans for the day
+    - Homework / Project deadlines
+}
+
+TODO:
+- Finish nutrition
+- Finish projects
+- Finish shifts
+- Finish schedule
+- Finish fitness
+********************************************************************************************************/
+
+/***
+MAIN
+****/
+
+console.log("Hello World!")
+
+/********
+NUTRITION
+*********/
+
+// Use lists for breakfast, lunch, dinner, and snacks
+// Choose a random number from 0 - (length of list) to get a random meal
+// Output those meals
+
+
+/*******
+PROJECTS
+********/
+
+
+
+/*****
+SHIFTS
+******/
+
+
+
+/*******
+SCHEDULE
+********/
+
+
+
+/******
+FITNESS
+*******/
+
+
